29522cd215b691de59d32943d4c4d81c1a771efb
Andy Apaez <andy.apaez16@gmail.com>
2025-09-26 22:51:21 -0400

    created a simple pay calculator using python fundamentals

---
+++ pay.py
@@ -0,0 +1,5 @@
+hourly_rate = input("Enter your hourly rate: ")
+hours_worked = input("Enter number of hours worked: ")
+pay = float(hourly_rate) * float(hours_worked)
+print("Your pay is: " + str(pay))
+# This program calculates the pay based on hourly rate and hours worked.
